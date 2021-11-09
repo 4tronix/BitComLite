8c162fa0417d4c580c5c8078201815e06af9d832
4tronix <Sales@4tronix.co.uk>
2021-11-09 12:18:44 +0000

    BCL07A

--- main.cpp
+++ main.cpp
@@ -13,11 +13,14 @@ namespace bitcommander
     // mount buttons on the pins with no pullup
     // TODO: fix this issue in the DAL itself
     #define ALLOC_PIN_BUTTON(id) new MicroBitButton(getPin(id)->name, id, MICROBIT_BUTTON_ALL_EVENTS);
+        ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P1)
+        ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P2)
+        ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P8)
         ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P12)
-        ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P16)
+        ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P13)
         ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P14)
         ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P15)
-        ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P8)
+        ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P16)
     #undef ALLOC_PIN_BUTTON
 
         initialized = true;
